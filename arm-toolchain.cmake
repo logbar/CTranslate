6d516e9286fa4d359d13292c1643273db7c41067
nakayama <nakayama@logbar.j~p>
2018-05-17 15:26:47 +0900

    armむけ、cmakeのmetadata

---
+++ arm-toolchain.cmake
@@ -0,0 +1,4 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc-4.9)
+SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-4.9)
+SET(CMAKE_INSTALL_PREFIX /cross/arm32/usr)
